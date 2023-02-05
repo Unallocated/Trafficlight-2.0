34a988eb794dfed3afecced383d9b093c5972d17
Corey Koval <ckoval7@gmail.com>
2023-02-05 01:46:02 -0500

    Refactor code

--- traffic.py
+++ traffic.py
@@ -9,46 +9,52 @@ app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 
 # Create a dictionary called pins to store the pin number, name, and pin state:
+# pins = {
+#     17 : {'name' : 'red', 'state' : GPIO.LOW},
+#     27 : {'name' : 'yellow', 'state' : GPIO.LOW},
+#     22 : {'name' : 'green', 'state' : GPIO.LOW},
+# }
+
 pins = {
-    17 : {'name' : 'red', 'state' : GPIO.LOW},
-    27 : {'name' : 'yellow', 'state' : GPIO.LOW},
-    22 : {'name' : 'green', 'state' : GPIO.LOW},
+    'red' : [17, GPIO.LOW],
+    'yellow' : [27, GPIO.LOW],
+    'green' : [22, GPIO.LOW]
 }
 
 
 # Set each pin as an output and make it low:
-for pin in pins:
+for pin, state in pins.values():
     # print(pin)
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
 
-def humanize_pin_state(pins):
-    for pin_num, pin_data in pins.items():
-        if pin_data['state'] == GPIO.LOW:
-            pin_data['state'] = 'off'
-        else:
-            pin_data['state'] = 'on'
-    return pins
+# def humanize_pin_state(pins):
+#     for pin_num, pin_state in pins.values():
+#         if pin_state == GPIO.LOW:
+#             pin_data['state'] = 'off'
+#         else:
+#             pin_data['state'] = 'on'
+#     return pins
 
 @app.route("/")
 def main():
-    # For each pin, read the pin state and store it in the pins dictionary:
-    for pin in pins:
-        pins[pin]['state'] = GPIO.input(pin)
+    getPinState()
     # Put the pin dictionary into the template data dictionary:
     templateData = {
-        'pins' : humanize_pin_state(pins)
+        'pins' : pins
         }
     # Pass the template data into the template main.html and return it to the user
     return render_template('main.html', **templateData)
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
-@app.route("/toggle/<changePin>/<action>")
-def action(changePin, action):
+# changePin is now color name
+@app.route("/toggle/<color>/<action>")
+def action(color, action):
     # Convert the pin from the URL into an integer:
-    changePin = int(changePin)
+    changePin = pins[color][0]
     # Get the device name for the pin being changed:
-    deviceName = pins[changePin]['name']
+    # deviceName = pins[changePin]['name']
+    deviceName = color
     # If the action part of the URL is "on," execute the code indented below:
     if action == "on":
         # Set the pin high:
@@ -64,17 +70,14 @@ def action(changePin, action):
         message = "Toggled " + deviceName + "."
     if action == "party":
         for i in range(1,20):
-            GPIO.output(changePin, GPIO.HIGH)
-            message = "Turned " + deviceName + " on."
+            blinkyBlink(changePin)
 
-    # For each pin, read the pin state and store it in the pins dictionary:
-    for pin in pins:
-        pins[pin]['state'] = GPIO.input(pin)
+    getPinState()
 
     # Along with the pin dictionary, put the message into the template data dictionary:
     templateData = {
         'message' : message,
-        'pins' : humanize_pin_state(pins)
+        'pins' : pins
     }
 
     return render_template('main.html', **templateData)
@@ -89,14 +92,19 @@ def partyHard(iterations = 5): #Add untested partyHard mode
     except:
         iterations = 5
     for i in range(iterations):
-        pin = choice(list(pins.keys()))
+        # pin = choice(list(pins.values()))
+        pin = choice(list(pins.values()))[0]
         blinkyBlink(pin)
     templateData = {
         # 'message' : message,
-        'pins' : humanize_pin_state(pins)
+        'pins' : pins
     }
     return render_template('main.html', **templateData)
 
+def getPinState():
+    # For each pin, read the pin state and store it in the pins dictionary:
+    for pin in pins:
+        pins[pin][1] = GPIO.input(pins[pin][0])
 
 def blinkyBlink(pin):
     GPIO.output(pin, GPIO.HIGH)
@@ -105,21 +113,22 @@ def blinkyBlink(pin):
     sleep(0.25)
 
 def goLow():
-    for i in pins: GPIO.output(i, GPIO.LOW)
+    for pin, state in pins: 
+        GPIO.output(pin, GPIO.LOW)
 
 def countDown():
     goLow()
     sleep(.25)
-    for i in pins:
-        GPIO.output(i, GPIO.HIGH)
+    for pin, state in pins:
+        GPIO.output(pin, GPIO.HIGH)
         sleep(1)
-        GPIO.output(i, GPIO.LOW)
+        GPIO.output(pin, GPIO.LOW)
 
-def rager(curr, prev):
-    for i in range(1,randint(4,16)):
-        curr = curr if curr != prev else choice(list(pins.keys())) #give it a second shot of being something else for a little more fun.
-        blinkyBlink(curr)
-    return curr
+# def rager(curr, prev):
+#     for i in range(1,randint(4,16)):
+#         curr = curr if curr != prev else choice(list(pins.keys())) #give it a second shot of being something else for a little more fun.
+#         blinkyBlink(curr)
+#     return curr
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=False)
